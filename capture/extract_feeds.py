7c468e3b398f700d7ab938cffdfedb86c9e2211f
Flávio Codeço Coelho <fccoelho@gmail.com>
2013-09-26 14:27:28 -0300

    Added script to extract feeds from list of urls

---
+++ capture/extract_feeds.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+u"""
+Created on 26/09/13
+by fccoelho
+license: GPL V3 or Later
+"""
+
+__docformat__ = 'restructuredtext en'
+
+
+import feedfinder
+import urlscanner
+import argparse
+
+def main(args):
+    with open(args.urls) as f:
+        for u in f:
+            print "searching for feeds in: ", u
+            feeds = feedfinder.feeds(u)
+            print "found %s feeds" % len(feeds)
+            if feeds:
+                feedfinder.store_feeds()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Search for feeds on a set of web pages (urls)')
+    parser.add_argument('file', metavar='urls', nargs=1,
+                        help='file with one or more urls to check (one per line)')
+
+    args = parser.parse_args()
+
+
---
+++ process/__init__.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+u"""
+Created on 26/09/13
+by fccoelho
+license: GPL V3 or Later
+"""
+
+__docformat__ = 'restructuredtext en'
