a5192f1a24b0feaf46440068c5beb181deabb5a9
Flávio Codeço Coelho <fccoelho@gmail.com>
2013-10-01 16:10:38 -0300

    fixed bug in downloader

--- capture/downloader.py
+++ capture/downloader.py
@@ -82,7 +82,7 @@ def parallel_fetch():
     feedurls = []
     for f in feeds:
         try:
-            feedurls.append(f["title_detail"]["base"])
+            feedurls.append(u''+f["title_detail"]["base"])
         except KeyError:
             print f
 
