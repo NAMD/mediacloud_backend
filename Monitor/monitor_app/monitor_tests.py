5165c02bb0b1aa1ac3e9e89f50f476ce3be8b0ba
Flavio C. Coelho <fccoelho@gmail.com>
2014-02-09 06:03:33 -0200

    fixed monitor_tests

--- Monitor/monitor_app/monitor_tests.py
+++ Monitor/monitor_app/monitor_tests.py
@@ -3,12 +3,8 @@ import os
 import json
 import unittest
 import tempfile
-
-<<<<<<< HEAD
-=======
 from app import app
 
->>>>>>> 25f11decbef25aeea196fd19187f3dfaddefe421
 
 class MonitorTestCase(unittest.TestCase):
 
@@ -24,27 +20,6 @@ class MonitorTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(app.config['DATABASE'])
 
-<<<<<<< HEAD
-    def test_articles_json_view(self):
-        rv = self.app.get("/articles/json")
-        self.assertIn('{"meta":', rv.data)
-
-    def test_feeds_json_view(self):
-        rv = self.app.get("/feeds/json")
-        self.assertIn('{"meta":', rv.data)
-
-    def test_urls_json_view(self):
-        rv = self.app.get("/urls/json")
-        self.assertIn('{"meta":', rv.data)
-
-    def test_mongo_query_with_empty_collections(self):
-        rv = self.app.get('/query/feeds')
-        self.assertIn('{"meta": {"count": 0}', rv.data)
-        rv = self.app.get('/query/articles')
-        self.assertIn('{"meta": {"count": 0}', rv.data)
-        rv = self.app.get('/query/urls')
-        self.assertIn('{"meta": {"count": 0}', rv.data)
-=======
     def test_fetch_docs_feeds(self):
         rv = self.app.get('/feeds/json')
         self.assertIn("aaData", json.loads(rv.data))
@@ -56,7 +31,7 @@ class MonitorTestCase(unittest.TestCase):
         self.assertGreater(len(json.loads(rv.data)["aaData"]), 0)
         rv = self.app.get('/query/urls')
         self.assertIn('{"meta": {"count":', rv.data)
->>>>>>> 25f11decbef25aeea196fd19187f3dfaddefe421
+
 
 if __name__ == '__main__':
     unittest.main()
