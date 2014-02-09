dbe96b66f2ae7e55b55e6e4e194913f73ff16338
Flavio C. Coelho <fccoelho@gmail.com>
2014-02-09 19:12:31 -0200

    incorporated @turicas parser of pt dates

--- utilities/parsedates.py
+++ utilities/parsedates.py
@@ -22,16 +22,50 @@ def parse_dates(collection):
             try:
                 collection.update({"_id": doc["_id"]}, {"%set": {"published": parse(doc['published'])}})
             except ValueError:
-                print "Could not parse string: {0:s}".format(doc['published'])
+                try:
+                    collection.update({"_id": doc["_id"]}, {"%set": {"published": parse_pt_date(doc['published'])}})
+                except:
+                    print "Could not parse string: {0:s}".format(doc['published'])
             except TypeError:
-                print "Could not parse string: {0:s}".format(doc['published'])
+                try:
+                    collection.update({"_id": doc["_id"]}, {"%set": {"published": parse_pt_date(doc['published'])}})
+                except:
+                    print "Could not parse string: {0:s}".format(doc['published'])
         if "updated" in doc and not isinstance(doc['updated'], datetime.datetime):
             try:
                 collection.update({"_id": doc["_id"]}, {"%set": {"updated": parse(doc['updated'])}})
             except ValueError:
-                print "Could not parse string: {0:s}".format(doc['updated'])
+                try:
+                    collection.update({"_id": doc["_id"]}, {"%set": {"updated": parse_pt_date(doc['updated'])}})
+                except:
+                    print "Could not parse string: {0:s}".format(doc['updated'])
             except TypeError:
-                print "Could not parse string: {0:s}".format(doc['published'])
+                try:
+                    collection.update({"_id": doc["_id"]}, {"%set": {"updated": parse_pt_date(doc['updated'])}})
+                except:
+                    print "Could not parse string: {0:s}".format(doc['updated'])
+
+MONTHS = {'jan': 1, 'fev': 2, 'mar': 3, 'abr': 4,  'mai': 5,  'jun': 6,
+          'jul': 7, 'ago': 8, 'set': 9, 'out': 10, 'nov': 11, 'dez': 12}
+
+def parse_pt_date(date_string):
+    '''Parses a date-time string and return datetime object
+       The format is like this:
+       'Seg, 21 Out 2013 22:14:36 -0200'
+    '''
+    date_info = date_string.split()
+    _, day, month_pt, year, hour_minute_second, offset = date_info
+    month = MONTHS[month_pt.lower()]
+    datetime_iso = '{}-{:02d}-{:02d}T{}'.format(year, month, int(day),
+            hour_minute_second)
+    datetime_object = datetime.datetime.strptime(datetime_iso, '%Y-%m-%dT%H:%M:%S')
+    offset_signal = int(offset[0] + '1')
+    offset_hours = int(offset[1:3])
+    offset_minutes = int(offset[3:5])
+    total_offset_seconds = offset_signal * (offset_hours * 3600 +
+                                            offset_minutes * 60)
+    offset_in_days = total_offset_seconds / (3600.0 * 24)
+    return datetime_object - datetime.timedelta(offset_in_days)
 
 if __name__ == "__main__":
     ## Media Cloud database setup
