7cacd108c9b8e5cdaae656f60019488d7444e56f
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-02-17 09:36:30 -0300

    incorporated new parse_pt_date version by @turicas

--- utilities/parsedates.py
+++ utilities/parsedates.py
@@ -2,7 +2,7 @@
 """
 This module helps migrating dates from strings to date objetcs in the article collection in MDCB
 Created on 23/10/13
-by fccoelho
+by fccoelho with contribution of parse_pt_date by @turicas
 license: GPL V3 or Later
 """
 
@@ -47,25 +47,46 @@ def parse_dates(collection):
 
 MONTHS = {'jan': 1, 'fev': 2, 'mar': 3, 'abr': 4,  'mai': 5,  'jun': 6,
           'jul': 7, 'ago': 8, 'set': 9, 'out': 10, 'nov': 11, 'dez': 12}
+FULL_MONTHS = {'janeiro': 1,  'fevereiro': 2, u'março': 3,    'abril': 4,
+               'maio': 5,     'junho': 6,     'julho': 7,     'agosto': 8,
+               'setembro': 9, 'outubro': 10,  'novembro': 11, 'dezembro': 12}
 
 def parse_pt_date(date_string):
     '''Parses a date-time string and return datetime object
        The format is like this:
        'Seg, 21 Out 2013 22:14:36 -0200'
     '''
-    date_info = date_string.split()
-    _, day, month_pt, year, hour_minute_second, offset = date_info
-    month = MONTHS[month_pt.lower()]
-    datetime_iso = '{}-{:02d}-{:02d}T{}'.format(year, month, int(day),
-            hour_minute_second)
-    datetime_object = datetime.datetime.strptime(datetime_iso, '%Y-%m-%dT%H:%M:%S')
-    offset_signal = int(offset[0] + '1')
-    offset_hours = int(offset[1:3])
-    offset_minutes = int(offset[3:5])
-    total_offset_seconds = offset_signal * (offset_hours * 3600 +
-                                            offset_minutes * 60)
-    offset_in_days = total_offset_seconds / (3600.0 * 24)
-    return datetime_object - datetime.timedelta(offset_in_days)
+    date_info = date_string.lower().split()
+    if date_info.count('de') == 2 or len(date_info) == 3:
+        if ',' in date_info[0]:
+            date_string = date_string.split(',')[1]
+        date_info = date_string.lower().replace('de ', '').split()
+        day, month_pt, year = date_info
+        if len(month_pt) == 3:
+            month = MONTHS[month_pt]
+        else:
+            month = FULL_MONTHS[month_pt]
+        date_iso = '{}-{:02d}-{:02d}'.format(year, int(month), int(day))
+        date_object = datetime.datetime.strptime(date_iso, '%Y-%m-%d')
+        return date_object
+    else:
+        _, day, month_pt, year, hour_minute_second, offset = date_info
+
+        if offset.lower() == 'gmt':
+            offset = '+0000'
+        offset_signal = int(offset[0] + '1')
+        offset_hours = int(offset[1:3])
+        offset_minutes = int(offset[3:5])
+        total_offset_seconds = offset_signal * (offset_hours * 3600 +
+                                                offset_minutes * 60)
+        offset_in_days = total_offset_seconds / (3600.0 * 24)
+
+        month = MONTHS[month_pt]
+        datetime_iso = '{}-{:02d}-{:02d}T{}'.format(year, month, int(day),
+                hour_minute_second)
+        datetime_object = datetime.datetime.strptime(datetime_iso,
+                '%Y-%m-%dT%H:%M:%S')
+        return datetime_object - datetime.timedelta(offset_in_days)
 
 if __name__ == "__main__":
     ## Media Cloud database setup
