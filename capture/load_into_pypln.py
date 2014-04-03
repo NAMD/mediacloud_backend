e9f94246a909ea0a4709e267a5fda7bbc3a04ae9
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-04-03 16:27:04 -0300

    incomplete

--- capture/load_into_pypln.py
+++ capture/load_into_pypln.py
@@ -7,4 +7,20 @@ license: GPL V3 or Later
 
 __docformat__ = 'restructuredtext en'
 
+import pymongo
 
+import nlp
+import settings
+
+
+client = pymongo.MongoClient(host=settings.MONGOHOST)
+
+articles = client.MCDB.articles
+corpus = nlp.get_corpus()
+
+article_count = articles.count()
+art_loaded = 0
+while art_loaded < article_count:
+    cursor = articles.find({}, limit=100)
+    for doc in cursor:
+        corpus.add_document(doc['link_content'])
