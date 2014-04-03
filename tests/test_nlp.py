65f45e6747b353b1a86fbc8537bd47545fd4213b
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-04-03 10:04:12 -0300

    started to add tests for nlp module

--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -6,3 +6,18 @@ license: GPL V3 or Later
 """
 
 __docformat__ = 'restructuredtext en'
+import unittest
+import subprocess
+import json
+from pypln.api import PyPLN, Corpus, Document
+
+
+from capture import nlp
+
+class TestNlp(unittest.TestCase):
+    def setUp(self):
+        self.corpus = nlp.get_corpus()
+        self.test_documents = []
+    def tearDown(self):
+        for doc in self.test_documents:
+            nlp.pypln.
