ed856a518f33457065286c9e160ab21a5fad0703
Flavio C. Coelho <fccoelho@gmail.com>
2014-04-23 14:50:08 -0300

    Added script to calculate the hot topics; Added script to extract freqdist from index.

--- analysis/htod.py
+++ analysis/htod.py
@@ -6,8 +6,17 @@ __author__ = 'fccoelho'
 
 import argparse
 import datetime
+import pymongo
+from pypln.api import PyPLN
 
 Today = datetime.datetime.today()
+
+def get_htod(d):
+    end = Today + datetime.timedelta(1)
+    arts = ARTICLES.find({"published": {"$gte": Today, "$lt": end}}, fields=["pypln_url"])
+    for article in arts:
+        pass
+
 
 
 if __name__ == "__main__":
@@ -15,3 +24,18 @@ if __name__ == "__main__":
 
     parser.add_argument("-d", "--date", type=str, default="{}-{}-{}".format(Today.year, Today.month, Today.day),
             help="Date to Analyse in YEAR-MO-DD format")
+    parser.add_argument("-h", '--host', type=str, help='MongoDB host to connect to')
+    parser.add_argument("-p", '--port', type=int, default=27017, help='MongoDB port to connect to')
+    parser.add_argument("--pyplhost", type=str, help="PyPLN host to use.")
+    parser.add_argument("--pyplnuser", type=str, help="PyPLN user.")
+    parser.add_argument("--pyplnpassword", type=str, help="PyPLN password")
+
+    args = parser.parse_args()
+
+    client = pymongo.MongoClient(args.host, args.port)
+    MCDB = client.MCDB
+    FEEDS = MCDB.feeds  # Feed collection
+    ARTICLES = MCDB.articles  # Article Collection
+    pypln = PyPLN(args.pyplnhost, (args.pyplnuser, args.pyplnpassword))
+
+    get_htod(args.date)
