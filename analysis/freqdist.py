7e8967b544f21debe78a325743ad64aabc99faa5
Flavio C. Coelho <fccoelho@gmail.com>
2014-04-23 11:10:37 -0300

    Added script to calculate the hot topics; Added script to extract freqdist from index.

--- analysis/freqdist.py
+++ analysis/freqdist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This Script uses the sphinxsearch index to obtain a list of stopwords, based on frequency analyses
+This Script uses the sphinxsearch index to obtain a list of words and their frequencies
 """
 __author__ = 'fccoelho'
 
@@ -8,7 +8,7 @@ __author__ = 'fccoelho'
 import argparse
 import os
 
-def generate_sw(dic, conf, ind, freq):
+def generate_freqdist(dic, conf, ind, freq):
     os.system("indexer --buildstops {} {} --buildfreqs {} -c {}".format(dic, freq, ind, conf))
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--dict", type=str, default="dict.txt", help="Dictionary file to be generated.")
     parser.add_argument("-c", "--conf", type=str, help="Path to the sphinx configuration file.")
     parser.add_argument("-i", "--index", type=str, help="Index to process.")
-    parser.add_argument("-f", "--frequency",type=int, default=100000, help="Frequency cutoff to use.")
+    parser.add_argument("-f", "--frequency", type=int, default=100000, help="Frequency cutoff to use.")
 
     args = parser.parse_args()
-    generate_sw(args.dict, args.conf, args.index, args.frequency)
+    generate_freqdist(args.dict, args.conf, args.index, args.frequency)
