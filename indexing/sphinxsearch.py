9e1ad67aab44e4b88c65c0ede83a79e23bda42c6
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-05-23 09:48:39 -0300

    Added title as an attribute to the article index

--- indexing/sphinxsearch.py
+++ indexing/sphinxsearch.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Perform a query on sphinxsearch and return it as a json object')
     parser.add_argument('--host', '-H', type=str, default='127.0.0.1', help="Searchd Host")
     parser.add_argument('--port', '-p', type=int, default=9312, help="port")
-    parser.add_argument('--index', '-i', required=False, default="MCDB_ARTICLES", help="Index")
+    parser.add_argument('--index', '-i', required=False, default="mediacloud_articles", help="Index")
     parser.add_argument('--query', '-q', required=True, type=str, nargs="+", help="query words")
     args = parser.parse_args()
 
