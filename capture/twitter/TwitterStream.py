c3f32195a2a5bbcae4cfe4a7330a6e339803e81c
Flavio Amieiro <amieiro.flavio@gmail.com>
2014-09-09 16:29:51 -0300

    Saves created_at also as a timestamp in the `tweets` collection

--- capture/twitter/TwitterStream.py
+++ capture/twitter/TwitterStream.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #coding:utf8
 
+import dateutil.parser
 import logging
 import json
 
@@ -59,8 +60,9 @@ class Filteredcapture(StreamListener):
     This is a basic listener that just prints received tweets to stdout.
     """
     def on_data(self, data):
-        # print data, type(data)
-        coll.insert(json.loads(data), w=1)
+        parsed_data = json.loads(data)
+        parsed_data['created_at_timestamp'] = dateutil.parser.parse(parsed_data['created_at']).strftime('%s')
+        coll.insert(parsed_data, w=1)
         return True
 
     def on_error(self, status):
