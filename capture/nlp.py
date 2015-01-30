38091fbbf9651b2d6ab85361adcb0f78cc947805
Elisa Mussumeci <elisamussumeci@gmail.com>
2015-01-30 19:11:16 -0200

    Change send_to_pypln function to send the 'cleaned_text' to pypln instead of 'link_content'.

--- capture/nlp.py
+++ capture/nlp.py
@@ -21,11 +21,12 @@ pypln = PyPLN(settings.PYPLNHOST, settings.PYPLN_CREDENTIALS)
 
 ARTICLES.ensure_index([("pypln_url", pymongo.ASCENDING)], sparse=True)
 
-def get_corpus(corpus_name='MC_articles'):
+
+def get_corpus():
     """
     Return the existing Mediacloud corpus or create it and return.
-    :rtype : Corpus object
     """
+    corpus_name = settings.CORPUS_NAME
     try:
         article_corpus = pypln.add_corpus(name=corpus_name, description='MediaCloud Articles')
     except RuntimeError:
@@ -38,8 +39,16 @@ def send_to_pypln(downloaded_article, corpus):
     """
     Takes a mediacloud document from the articles collection and insert into a pypln corpus.
     """
-    pypln_document = corpus.add_document(
-            decompress_content(downloaded_article['link_content']))
+    if 'cleaned_text' in downloaded_article:
+        if len(downloaded_article['cleaned_text']) > 0:
+            pypln_document = corpus.add_document(
+                    decompress_content(downloaded_article['cleaned_text']))
+    elif 'summary' in downloaded_article:
+        pypln_document = corpus.add_document(
+                decompress_content(downloaded_article['summary']))
+    else:
+        pypln_document = corpus.add_document(
+                decompress_content(downloaded_article['title']))
     return pypln_document
 
 
