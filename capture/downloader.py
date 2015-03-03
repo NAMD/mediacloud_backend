8a8b2fe4d30b6a5ad85624b3f2a88fc3c6dd6637
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-03-03 13:45:26 -0300

    Comments out code that sends articles to pypln during download
    Because of an error in pypln it is timing out when under heavy load. This means
    that Mediacloud sending every article to pypln is bringing it down.
    
    This commit is meant to be temporary, just until we fix the bug in pypln and
    bring it back to normal.

--- capture/downloader.py
+++ capture/downloader.py
@@ -32,8 +32,8 @@ from dateutil.parser import parse
 import settings
 import elasticsearch
 
-import nlp
-import load_into_pypln
+#import nlp
+#import load_into_pypln
 
 es = elasticsearch.Elasticsearch(hosts=[settings.ELASTICHOST])
 
@@ -112,7 +112,7 @@ class RSSDownload(object):
 
     def _save_articles(self, entries):
         logger.info("Downloading %s articles from %s", len(entries), self.url)
-        corpus = nlp.get_corpus()
+        #corpus = nlp.get_corpus()
         for entry in entries:
             if "%set" in entry:  # hallmark of empty article
                 logger.error("Empty article from %s", self.url)
@@ -169,7 +169,7 @@ class RSSDownload(object):
                 entry.pop('published_parsed')
             except KeyError:
                 pass
-            load_into_pypln.load_document(entry, corpus)
+            #load_into_pypln.load_document(entry, corpus)
             exists = list(ARTICLES.find({"link": entry.link}))
             # print exists
             if not exists:
