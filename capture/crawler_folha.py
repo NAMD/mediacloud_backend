c213c3e26edc87d5151e63381df9f11b18cc64d7
lucasmachadorj <lucasmachado.rj@gmail.com>
2015-07-27 20:40:10 -0300

    method to extract last news' urls

--- capture/crawler_folha.py
+++ capture/crawler_folha.py
@@ -1,9 +1,60 @@
-from bs4 import BeautifulSoup
+import sys
+import pymongo
+import logging
+import settings
+import datetime
 import requests
 
-INDEX_URL = "http://www1.folha.uol.com.br/ultimas-noticias/index.shtml"
+from goose import Goose
+from bs4 import BeautifulSoup
+from logging.handlers import RotatingFileHandler
 
-index = requests.get(INDEX_URL).content
-soup = BeautifulSoup(index)
-news_index = soup.find(**{'class': 'news-index'}).find('ol')
-news_urls = set([n.attrs['href'] for n in news_index.find_all('a')])
+
+###########################
+#  Setting up Logging
+###########################
+
+logger = logging.getLogger("Folha de Sao Paulo")
+logger.setLevel(logging.DEBUG)
+
+# create stream handler and set level to debug
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+file_handler = RotatingFileHandler('/tmp/mediacloud_folha_sao_paulo.log',
+                          maxBytes=5e6,
+                          backupCount=3)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - \
+                               %(levelname)s - %(message)s')
+
+# add formatter to stream_handler
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# add stream_handler to logger
+logger.addHandler(stream_handler)  # uncomment for console output of messages
+logger.addHandler(file_handler)
+
+# client = pymongo.MongoClient(settings.MONGOHOST, 27017)
+# mcdb = client.MCDB
+# ARTICLES = mcdb.articles  # Article Collection
+# ARTICLES.ensure_index("source")
+
+
+def find_articles(page=None):
+
+    base_url = "http://www1.folha.uol.com.br/ultimas-noticias/"
+    if page is None:
+        INDEX_URL = base_url + "index.shtml"
+    else:
+        INDEX_URL = base_url + "noticias-{0}.shtml".format(page)
+
+    print(INDEX_URL)
+    index = requests.get(INDEX_URL).content
+    soup = BeautifulSoup(index)
+    news_index = soup.find(**{'class': 'news-index'}).find('ol')
+    news_urls = [url.attrs['href'] for url in news_index.find_all('a')]
+    return news_urls
+
+
