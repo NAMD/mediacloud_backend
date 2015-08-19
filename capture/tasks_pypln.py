9700643eb36315d54cb6752ae81be0cc609ed56c
Elisa Mussumeci <elisamussumeci@gmail.com>
2015-08-18 23:42:29 -0300

    Changes the backend argument

--- capture/tasks_pypln.py
+++ capture/tasks_pypln.py
@@ -11,7 +11,7 @@ pypln_temp = client.MCDB.pypln_temp # pypln_temp temporary collection
 articles_analysis = client.MCDB.articles_analysis # articles_analysis collection
 
 
-app = Celery('tasks', backend="mongodb")
+app = Celery('tasks', backend='redis://localhost')
 
 @app.task(bind=True)
 def fetch_property(self, _id):
