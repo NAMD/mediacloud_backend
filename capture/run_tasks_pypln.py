6bb80f7870aea18145fbae7a3f3a7d4fbffa2b62
Elisa Mussumeci <elisamussumeci@gmail.com>
2015-08-19 15:34:32 -0300

    Changes the name of the file

---
+++ capture/run_tasks_pypln.py
@@ -0,0 +1,28 @@
+from celery import Celery
+import pypln.api
+import pymongo
+import settings
+from tasks_pypln import fetch_property
+from celery import group
+
+
+## Media Cloud database setup
+client = pymongo.MongoClient(host=settings.MONGOHOST)
+articles = client.MCDB.articles # articles collection
+pypln_temp = client.MCDB.pypln_temp # pypln_temp temporary collection
+articles_analysis = client.MCDB.articles_analysis # articles_analysis collection
+
+
+def get_pypln_properties(doc_id):
+    article = pypln_temp.find_one({'_id': doc_id})
+    articles_analysis.insert({'articles_id': article['articles_id']})
+
+    results = fetch_property.delay(doc_id)
+
+    pypln_temp.remove({'_id': doc_id})
+
+
+
+for article in pypln_temp.find():
+    doc_id = article['_id']
+    get_pypln_properties(doc_id)
